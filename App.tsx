844102416ab01bbe18c561942bf77df9e8f09a7d
Emil Svedrin <emil.svedrin@gmail.com>
2022-04-11 12:49:09 +0100

    kmom01

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Stock from './components/Stock.tsx';
-import warehouse from './assets/warehouse.jpg';
+import Stock from './components/Stock';
+// import icon from './assets/icon.jpg';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.base}>
-        <Text style={{color: '#33c', fontSize: 42}}>Lager-Appen</Text>
-        {<Image source={warehouse} style={{ width: 320, height: 240 }} />}
+        <Text style={{color: '#ffffff', fontSize: 42}}>Lager Appen</Text>
+        {/* {<Image source={icon} style={{ width: 320, height: 240 }} />} */}
         <Stock />
         <StatusBar style="auto" />
       </View>
@@ -23,7 +23,9 @@ const styles = StyleSheet.create({
   },
   base: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000e47',
+    color: '#fff',
+    // backgroundColor: '#fff',
     paddingLeft: 12,
     paddingRight: 12,
   }
